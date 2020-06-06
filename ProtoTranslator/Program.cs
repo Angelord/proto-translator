02278eef5c341cec42f6f22b945237634fcd20e5
Angelord <Georgiev1997@gmail.com>
2020-06-06 14:17:45 +0300

    Add source file selection to compiler.

--- ProtoTranslator/Program.cs
+++ ProtoTranslator/Program.cs
@@ -1,18 +1,30 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Diagnostics;
 
 namespace ProtoTranslator {
     internal class Program {
 
+        private const string SOURCE_FOLDER = "Resources/";
+        private const string BIN_FOLDER = "Builds/";
+
         public static string ExecutionDir => Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName);
 
         public static void Main(string[] args) {
 
-            Translator translator = new Translator("Resources/Program.c", "Result/GenProgram_IncrTest");
+            Console.WriteLine("Select the input program : ");
+            string inputProgramName = Console.ReadLine();
+            string sourceProgramPath = $"{SOURCE_FOLDER}{inputProgramName}.c";
+            string destProgramPath = $"{BIN_FOLDER}{inputProgramName}";
+            
+            if (!File.Exists(sourceProgramPath)) {
+                Console.WriteLine("Program not found!");
+                return;
+            }
 
+            Translator translator = new Translator(sourceProgramPath, destProgramPath);
             translator.LogLexer = true;
             translator.LogSyntaxTree = true;
-            
             translator.Translate();
         }
     }
